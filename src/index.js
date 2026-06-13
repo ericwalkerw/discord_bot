6ced6f5059c370f2334e672b06e6994039646f41
ericwalkere <ericwalker.leo@gmail.com>
2026-06-13 14:57:14 +0700

    add version

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import MainController from "./MainController.js";
 import Emitter from "./Common/Emitter.js";
 import Brain from "./Brain.js";
 import Speech from "./Speech.js";
+const ver = "0.0.1";
 class AIAgent {
     constructor() {
         this.onLoad();
     }
     
     onLoad() {
+        console.log("BOT version: ", ver);
         console.log("GENERAL INIT MODULE");
         Emitter.instance = new Emitter();
         Brain.instance = new Brain();
